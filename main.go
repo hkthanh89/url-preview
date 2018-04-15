0ec7b8c468d5ae47511a9f044e0bb242f2f644fa
Thanh Huynh Kien <hkthanh89@gmail.com>
2018-04-15 17:11:39 +0700

    Get preview information of url

--- main.go
+++ main.go
@@ -1,99 +1,113 @@
 package main
 
 import (
-  "encoding/json"
-  "log"
-  "net/http"
-  "github.com/gorilla/mux"
-  "strings"
-  "fmt"
-  "github.com/PuerkitoBio/goquery"
+	"encoding/json"
+	_ "fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"strings"
 )
 
 type UrlPreview struct {
-  Url   string `json:"url"`
-  Title string `json:"title"`
+	Url         string `json:"url"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Image       string `json:"image"`
 }
 
 type Result struct {
-  UrlPreview UrlPreview `json:"object"`
+	UrlPreview UrlPreview `json:"object"`
 }
 
 type Response struct {
-  Code   int `json:"code"`
-  Result Result `json:"result"`
+	Code   int    `json:"code"`
+	Result Result `json:"result"`
 }
 
 type ErrorResponse struct {
-  Code int `json:"code"`
-  Error string `json:"error"`
+	Code  int    `json:"code"`
+	Error string `json:"error"`
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-  query := r.URL.Query()
-  url := query["url"][0]
+	query := r.URL.Query()
+	url := query["url"][0]
 
-  if !strings.Contains(url, "http") {
-    url = "http://" + url
-  }
+	if !strings.Contains(url, "http") {
+		url = "http://" + url
+	}
 
-  // Get html
-  res, err := http.Get(url)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer res.Body.Close()
+	// Get html
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
 
-  // Server response
-  var response interface{}
-  if res.StatusCode == 200 { // Valid url
-    html, err := goquery.NewDocumentFromReader(res.Body)
-    if err != nil {
-      log.Fatal(err)
-    }
+	// Server response
+	var response interface{}
+	if res.StatusCode == 200 { // Valid url
+		html, err := goquery.NewDocumentFromReader(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-    // Find information
-    title := html.Find("title").First()
+		var title, description, image string
 
-    meta := html.Find("meta")
-    for _, node := range meta.Nodes {
-      // Loop nodes
-      fmt.Printf("node=%s attributes=%s \n", node.Data, node.Attr)
-      // for _, attr := range node.Attr {
-      //   // Loop attributes of a node.
-      //   fmt.Printf("Key=%s Value=%s Data=%s \n", attr.Key, attr.Val, node.Data)
-      // }
-    }
+		meta := html.Find("meta")
+		for _, node := range meta.Nodes {
+			// Loop nodes
+			doc := goquery.NewDocumentFromNode(node)
+			val, exists := doc.Selection.Attr("property")
 
-    response = Response{
-      res.StatusCode,
-      Result{
-        UrlPreview{
-          Url: url,
-          Title: title.Text(),
-        },
-      },
-    }
-  } else { // Invalid url
-    response = ErrorResponse{
-      400,
-      "Invalid URL",
-    }
-  }
+			if exists == true {
+				if val == "og:title" {
+					title, _ = doc.Selection.Attr("content")
+				}
 
-  data, err := json.Marshal(response)
-  if err != nil {
-    log.Fatal(err)
-  }
+				if val == "og:description" {
+					description, _ = doc.Selection.Attr("content")
+				}
 
-  w.Header().Set("Content-Type", "application/json")
-  w.WriteHeader(200)
-  w.Write(data)
+				if val == "og:image" {
+					image, _ = doc.Selection.Attr("content")
+				}
+			}
+		}
+
+		response = Response{
+			res.StatusCode,
+			Result{
+				UrlPreview{
+					Url:         url,
+					Title:       title,
+					Description: description,
+					Image:       image,
+				},
+			},
+		}
+	} else { // Invalid url
+		response = ErrorResponse{
+			400,
+			"Invalid URL",
+		}
+	}
+
+	data, err := json.Marshal(response)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(data)
 }
 
 func main() {
-  router := mux.NewRouter()
-  router.HandleFunc("/", HomeHandler).Methods("GET")
+	router := mux.NewRouter()
+	router.HandleFunc("/", HomeHandler).Methods("GET")
 
-  log.Fatal(http.ListenAndServe(":8889", router))
+	log.Fatal(http.ListenAndServe(":8889", router))
 }
