13e7f733f2aeb4ac6c57462343c2c75dbf544b6a
Thanh Huynh <hkthanh89@users.noreply.github.com>
2024-05-03 19:30:53 +0700

    return reading time of webpage

--- app/models/url_preview.go
+++ app/models/url_preview.go
@@ -1,8 +1,9 @@
 package models
 
 type UrlPreview struct {
-  Url         string `json:"og:url"`
-  Title       string `json:"og:title"`
-  Description string `json:"og:description"`
-  Image       string `json:"og:image"`
+	Url         string `json:"og:url"`
+	Title       string `json:"og:title"`
+	Description string `json:"og:description"`
+	Image       string `json:"og:image"`
+	ReadingTime string `json:"og:reading_time"`
 }
--- app/services/page/page.go
+++ app/services/page/page.go
@@ -1,42 +1,55 @@
 package page
 
 import (
-  "io"
-  "github.com/PuerkitoBio/goquery"
-  "github.com/hkthanh89/url-preview/app/models"
-  "github.com/hkthanh89/url-preview/app/utils"
+	"fmt"
+	"io"
+	"math"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/hkthanh89/url-preview/app/models"
+	"github.com/hkthanh89/url-preview/app/utils"
 )
 
 func GetPreviewInfo(r io.Reader) (models.UrlPreview, error) {
-  document, err := goquery.NewDocumentFromReader(r)
-  urlPreview := models.UrlPreview{}
+	document, err := goquery.NewDocumentFromReader(r)
+	urlPreview := models.UrlPreview{}
 
-  if err != nil {
-    return urlPreview, err
-  }
+	if err != nil {
+		return urlPreview, err
+	}
 
-  document.Find("meta").Each(func(i int, s *goquery.Selection) {
-    if s.AttrOr("property", "") == "og:title" {
-      urlPreview.Title = attrContent(s)
-    }
+	document.Find("meta").Each(func(i int, s *goquery.Selection) {
+		if s.AttrOr("property", "") == "og:title" {
+			urlPreview.Title = attrContent(s)
+		}
 
-    if (s.AttrOr("name", "") == "description") || (s.AttrOr("property", "") == "og:description") {
-      urlPreview.Description = attrContent(s)
-    }
+		if (s.AttrOr("name", "") == "description") || (s.AttrOr("property", "") == "og:description") {
+			urlPreview.Description = attrContent(s)
+		}
 
-    if s.AttrOr("property", "") == "og:image" {
-      urlPreview.Image = attrContent(s)
-    }
-  })
+		if s.AttrOr("property", "") == "og:image" {
+			urlPreview.Image = attrContent(s)
+		}
+	})
 
-  // Get missing data
-  if utils.Blank(urlPreview.Title) {
-    urlPreview.Title = document.Find("head > title").First().Text()
-  }
+	// Get missing data
+	if utils.Blank(urlPreview.Title) {
+		urlPreview.Title = document.Find("head > title").First().Text()
+	}
 
-  return urlPreview, nil
+	wordsCount := 0
+	document.Find("p").Each(func(i int, s *goquery.Selection) {
+		wordsCount += len(strings.Split(s.Text(), " "))
+	})
+
+	wordsPerMinute := 200
+	minutesReading := math.Ceil(float64(wordsCount) / float64(wordsPerMinute))
+	urlPreview.ReadingTime = fmt.Sprintf("%d min", int(minutesReading))
+
+	return urlPreview, nil
 }
 
 func attrContent(s *goquery.Selection) string {
-  return s.AttrOr("content", "")
+	return s.AttrOr("content", "")
 }
